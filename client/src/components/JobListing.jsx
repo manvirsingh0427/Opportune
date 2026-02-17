53a9c398c3363c576d9d7937fbb0675c7d77ad81
manvirsingh0427 <manvirsinghmadhok@gmail.com>
2026-02-17 12:34:43 +0530

    add searchByLocation and jobsCard

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -1,9 +1,10 @@
 import React, { useContext } from 'react'
 import { AppContext } from '../context/AppContext'
-import { assets, JobCategories } from '../assets/assets'
+import { assets, JobCategories, JobLocations} from '../assets/assets'
+import JobCard from './JobCard'
 
 const JobListing = () => {
-    const {isSearched, searchFilter, setSearchFilter} = useContext(AppContext)
+    const {isSearched, searchFilter, setSearchFilter, jobs} = useContext(AppContext)
     const removeSearch = () => {
         setSearchFilter(prev=> ({
             ...prev, title:""
@@ -62,7 +63,35 @@ const JobListing = () => {
                 </ul>
             </div>
 
+            {/* Location Filter */}
+            <div className='max-lg:hidden'>
+                <h4 className='font-medium text-lg py-4 pt-14'>
+                    Search by Location
+                </h4>
+
+                <ul className='space-y-4 text-gray-600'>
+                    {
+                        JobLocations.map((location, index)=>(
+                            <li className='flex gap-3 items-center' key={index}>
+                                <input className='scale-125' type="checkbox" name="" id="" />
+                                {location}
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div>
         </div>
+
+        {/* Job listings */}
+        <section className='w-full lg-w-3/4 text-gray-800 max-lg:px-4'>
+            <h3 className='font-medium text-3xl py-2' id='job-list'>Latest jobs</h3>
+            <p className='mb-8'>Get your job from top companies</p>
+            <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4'>
+                 {jobs.map((job, index)=>(
+                     <JobCard key={index} job={job}/>
+                 ))}
+            </div>
+        </section>
     </div>
   )
 }
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { jobsData } from "../assets/assets";
 
 export const AppContext = createContext();
 
@@ -9,10 +10,21 @@ export const AppContextProvider = (props)=> {
     })
 
     const [isSearched, setIsSearched] = useState(false);
+    const [jobs, setJobs] = useState([]);
+
+    // function to fetch jobs data
+    const fetchJobs = async ()=> {
+          setJobs(jobsData)
+    }
+
+    useEffect(()=>{
+        fetchJobs()
+    }, [])
 
     const value = {
         setSearchFilter, searchFilter, 
         isSearched, setIsSearched,
+        jobs, setJobs
     }
 
     return (
