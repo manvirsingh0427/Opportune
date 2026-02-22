605b910b437f974374f7926febc08c09e2284f17
manvirsingh0427 <manvirsinghmadhok@gmail.com>
2026-02-22 15:39:26 +0530

    Complete ApplyJobs page

--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom'
 import {AppContext} from '../context/AppContext'
 import Loading from '../components/Loading'
 import Navbar from '../components/Navbar'
+import JobCard from '../components/JobCard'
+import Footer from '../components/Footer'
 import { assets } from '../assets/assets'
 import kConvert from 'k-convert'
 import moment from 'moment'
@@ -63,8 +65,26 @@ const ApplyJob = () => {
               <p className='mt-1 text-gray-600'>Posted {moment(JobData.date).fromNow()}</p>
             </div>
           </div>
+
+          <div className='flex flex-col lg:flex-row justify-between items-start'>
+            <div className='w-full lg:w-2/3'>
+              <h2 className='font-bold text-2xl mb-4'>Job description</h2>
+              <div className='text-gray-700' dangerouslySetInnerHTML={{__html:JobData.description}}></div>
+              <button className='bg-blue-600 p-2.5 px-10 text-white rounded mt-10'> Apply Now</button>
+            </div>
+
+            {/* Right Section More Jobs */}
+            <div className='w-full lg:w-1/3 mt-8 lg:mt-0 lg:ml-8 space-y-5'>
+               <h2>More Jobs from {JobData.companyId.name}</h2>
+               {jobs.filter(job => job._id != JobData._id && job.companyId._id === JobData.companyId._id)
+               .filter(job => true).slice(0, 4).map((job, index)=>
+               <JobCard key={index} job={job}/>)}
+            </div>
+          </div>
         </div>
        </div>
+
+       <Footer/>
     </>
   )
 
